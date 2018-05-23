8d96ec76fccacc69639fca6c76d9c6c4d2644933
vinchauh1 <vinayak.chauhan@digitas.com>
2018-05-23 11:41:53 +0530

    Sitecore Sitemap Module

---
+++ VShared.Sitemap/VShared.Sitemap/Models/UrlSet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Sitemap.Models
+{
+    ///<summary>
+    /// Class to generate urlset for sitemap
+    /// </summary>
+
+    [XmlRoot("urlset")]
+    public class Urlset
+    {
+        ///<summary>
+        /// Constructor to initialize Url Object
+        /// </summary>
+
+        public Urlset() { Url = new List<Url>(); }
+
+        ///<summary>
+        /// Urls collection
+        /// </summary>
+
+        [XmlElement("url")]
+        public List<Url> Url { get; set; }
+
+    }
+
+    ///<summary>
+    /// Class to generate url with its parameters for sitemap
+    /// </summary>
+
+    public class Url
+    {
+        ///<summary>
+        /// Location Parameter
+        /// </summary>
+
+        [XmlElement("loc")]
+        public string Loc { get; set; }
+
+        ///<summary>
+        /// Last modified on
+        /// </summary>
+
+        [XmlElement("lastmod")]
+        public string Lastmod { get; set; }
+
+        //Add required properties here like changefreq, priority
+    }
+}
